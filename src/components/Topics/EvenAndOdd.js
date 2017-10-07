3931f31db2fc0f4c1498bb9528b29ecd9dfe5cad
CHASE <t.chasepettyjohn@yahoo.com>
2017-10-06 17:34:19 -0700

    filterobject component finished

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -47,13 +47,4 @@ constructor(){
 
 export default EvenAndOdd;
 
-// Create an onChange prop for the input element that updates the value of userInput on state.
 
-// Create an onClick prop for the button element that calls a method on the class:
-// This method should solve the toy problem.
-// This method should update the value of evenArray and oddArray on state.
-
-
-
-// Assign one span element to display the value of evenArray.
-// Assign the other span element to display the value of oddArray.
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -3,13 +3,53 @@ import React, { Component } from 'react';
 
 
 class FilterObject extends Component {
+    constructor() {
+        super();
+        this.state = {
+                  employees: [{name: 'Jimmy Joe',
+                                title: 'Hack0r',
+                                age: 12, },
+                                {name: 'Jeremy Schrader',
+                                age: 24,
+                                hairColor: 'brown'},
+                                {name: 'Carly Armstrong',
+                                title: 'CEO',}],
+            userInput: '',
+            filteredEmployees: [],
+        }
+    }
+        updateUserInput(val){
+        this.setState({ userInput: val })
+    };
+    toyProblem(prop){
+
+        var filteredEmployees = [];
+        var employees = this.state.employees;
+
+        for ( var i=0; i<employees.length; i++) {
+            if(employees[i].hasOwnProperty(prop)) {
+            filteredEmployees.push(employees[i]);
+            }
+        }
+            this.setState({ filteredEmployees: filteredEmployees });
+        }
+
   render() {
       return(
-        <div>
-            <h1>filter object</h1>
+        <div className="puzzleBox filterPbjectPB">
+            <h1>Filter Object</h1>
+            <span className="puzzleText">{JSON.stringify(this.state.employees)}</span>
+            <input type="text" className="inputLine" onChange={ (e) => {this.updateUserinput(e.target.value); }}/>
+            <button className="confirmationButton" onClick={ () => { this.toyProblem(this.state.userInput); } }>Filter</button>
+            <span className="resultsBox filterObjectRB">{this.state.filteredEmployees}</span>
         </div>
        )
     }
 }
 
 export default FilterObject;
+
+
+
+// Assign the first span element the value of unFilteredArray.
+// Assign the last span element the value of filteredArray.
